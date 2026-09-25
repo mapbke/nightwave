fa0f3886930cd44cd68e4189f7bd1faca2febbbd
pussy-licker <157161122+mapbke@users.noreply.github.com>
2026-09-25 23:30:46 +0300

    v0.5: update dev launcher

--- run-dev.bat
+++ run-dev.bat
@@ -1,8 +1,8 @@
 @echo off
+setlocal
 cd /d "%~dp0"
-if not exist node_modules (
-  echo [Nightwave] Installing dependencies...
-  call npm install
-  if errorlevel 1 pause & exit /b 1
-)
+
+call npm install --no-audit --no-fund
+if errorlevel 1 exit /b 1
+
 call npm run dev
